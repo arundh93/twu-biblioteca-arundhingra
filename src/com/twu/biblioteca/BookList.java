a6bd317aa3f56b4e7e319bdf91ac2362615de6e4
arundh93 <arun.dh93@gmail.com>
2015-07-28 12:54:22 +0530

    Arun | #0005 | created BookList which accepts a list of books can return it

---
+++ src/com/twu/biblioteca/BookList.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+
+
+public class BookList {
+    private ArrayList<Book> bookList;
+
+    public BookList(ArrayList<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    public ArrayList<Book> getBookList() {
+        return bookList;
+    }
+}

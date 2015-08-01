4450c500d5144eb97792ff145dcd90f16aa58d63
arundh93 <arun.dh93@gmail.com>
2015-08-01 14:46:08 +0530

    Arun | #0040 | added functionality to retrieve the director of the movie

--- src/com/twu/biblioteca/Movie.java
+++ src/com/twu/biblioteca/Movie.java
@@ -2,12 +2,18 @@ package com.twu.biblioteca;
 
 public class Movie {
     private String movieName;
+    private String director;
 
-    public Movie(String movieName) {
+    public Movie(String movieName, String director) {
         this.movieName = movieName;
+        this.director = director;
     }
 
     public String getTitle() {
         return movieName;
+    }
+
+    public String getDirector() {
+        return director;
     }
 }
--- test/com/twu/biblioteca/MovieTest.java
+++ test/com/twu/biblioteca/MovieTest.java
@@ -4,12 +4,20 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+
 public class MovieTest {
 
     @Test
     public void canGetTitleOfMovie() {
-        Movie movie = new Movie("singham");
+        Movie movie = new Movie("singham", "roy");
 
         assertEquals("singham", movie.getTitle());
+    }
+
+    @Test
+    public void canGetAuthorOfBook() {
+        Movie movie = new Movie("singham", "roy");
+
+        assertEquals("roy", movie.getDirector());
     }
 }
